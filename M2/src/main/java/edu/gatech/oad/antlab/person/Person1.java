e34d357167cc75026c5400ee2a27b41e9a444520
David Arida <davidarida@lawn-128-61-38-37.lawn.gatech.edu>
2016-09-06 19:41:18 -0400

    Plz Work

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -1,3 +1,60 @@
+<<<<<<< HEAD:M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 1
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author DAVID ARIDA
+ *  @version 1.1
+ */
+public class Person1 {
+  /** Holds the persons real name */
+  private String name;
+  	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+  public Person1(String pname) {
+    name = pname;
+  }
+  	/**
+	 * This method should take the string
+	 * input and return its characters rotated
+	 * 2 positions.
+	 * given "gtg123b" it should return
+	 * "g123bgt".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+	  String result = input;
+	  if (input.length() > 0) {
+	  	for (int i = 0; i < 2; i++){
+	  		char character = input.charAt(0);
+	  		result = result.substring(1, input.length() - 1) + character;
+	  	}
+	  }
+	  return result;
+	}
+	
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+
+}
+=======
 package edu.gatech.oad.antlab.person;
 
 /**
@@ -47,3 +104,4 @@ public class Person1 {
 	}
 
 }
+>>>>>>> 594f33b1dc53abed562c724d61e9c84cb6f6b418:M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
