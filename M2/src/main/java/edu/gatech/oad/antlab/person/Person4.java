f914c53724ac26d1c938a0efc32fc2c481b4c25d
Alexandra Durso <adurso3@gatech.edu>
2016-09-06 22:11:42 -0400

    Fix Person4.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,8 +5,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string
  *
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Alexandra Durso
  *  @version 1.1
  */
@@ -36,9 +36,7 @@ public class Person4 {
       String str = "";
       for (int i = 0; i < input.length(); i++) {
           char c = input.charAt(i);
-          str = str + c++;
-        char c = str.charAt(i);
-        str = str + c++;
+          str = str + Character.toString(++c);
       }
       return str;
     }
