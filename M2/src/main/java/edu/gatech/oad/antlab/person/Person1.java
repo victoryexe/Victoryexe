655cf2478c01f9faa1c02df24a2a19d7e7bf5676
David Arida <davidarida@lawn-128-61-38-148.lawn.gatech.edu>
2016-09-09 16:26:22 -0400

    Edited paths in darida3.gradle, moved gradle file outside of build.gradle folder, and ensured Person1.java compiles

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
 package edu.gatech.oad.antlab.person;
 
 /**
@@ -6,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author DAVID ARIDA
+ *  @author Bob
  *  @version 1.1
  */
 public class Person1 {
@@ -54,54 +53,3 @@ public class Person1 {
 	}
 
 }
-=======
-package edu.gatech.oad.antlab.person;
-
-/**
- *  A simple class for person 1
- *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
- *  @version 1.1
- */
-public class Person1 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-  public Person1(String pname) {
-    name = pname;
-  }
-  	/**
-	 * This method should take the string
-	 * input and return its characters rotated
-	 * 2 positions.
-	 * given "gtg123b" it should return
-	 * "g123bgt".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
-	}
-	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
-
-}
->>>>>>> 594f33b1dc53abed562c724d61e9c84cb6f6b418:M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
