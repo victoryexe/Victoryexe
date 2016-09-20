c9c0a9350cd10550b1fad1b5213b461b8fb80b19
grizzlyThresher <grizzlyThresher@gmail.com>
2016-09-20 12:47:14 -0400

    Edited Main file to get Stage set with Login screen

--- WaterReport/src/controller/LoginScreenController.java
+++ WaterReport/src/controller/LoginScreenController.java
@@ -1,7 +1,26 @@
 package controller;
 
-/**
- * Created by grizz on 9/19/2016.
- */
+
+import fxapp.Main;
+import javafx.fxml.FXML;
+
 public class LoginScreenController {
+
+    /** a link back to Main */
+    private Main mainApp;
+
+    /**
+     * allows for calling back to the main code if necessary
+     * @param main  the reference to the FX Application instance
+     */
+    public void setMainApp(Main main) { mainApp = main; }
+
+    /**
+     * Close menu item event handler
+     */
+    @FXML
+    private void handleCloseMenu() {
+        System.exit(0);
+
+    }
 }
--- WaterReport/src/fxapp/Main.java
+++ WaterReport/src/fxapp/Main.java
@@ -1,20 +1,63 @@
 package fxapp;
 
+import controller.LoginScreenController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Main application class.
+ */
+
+public class Main extends Application {
+    /** the java logger for this class */
+    private static final Logger LOGGER =Logger.getLogger("Main");
+
+    /** main container for application */
+    private Stage screen;
+
+    private AnchorPane rootLayout;
     @Override
     public void start(Stage primaryStage) throws Exception{
-
+        screen = primaryStage;
+        initRootLayout(screen);
     }
 
+    public Stage getScreen() { return screen;}
+
+    private void initRootLayout(Stage mainScreen) {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("../view/LoginScreenView.fxml"));
+            rootLayout = loader.load();
+
+            //Setting controller
+            LoginScreenController controller = loader.getController();
+
+            //Setting title
+            mainScreen.setTitle("Aqua Pura");
+
+            //shows the scene with rootLayout
+            Scene scene = new Scene(rootLayout);
+            mainScreen.setScene(scene);
+            mainScreen.show();
+        } catch (IOException e) {
+            //error on load, must be logged
+            LOGGER.log(Level.SEVERE, "Failed to find the FXML file!");
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
