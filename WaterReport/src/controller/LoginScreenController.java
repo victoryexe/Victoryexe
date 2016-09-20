8c3f98200475672415f2883736e6814fd328485e
grizzlyThresher <grizzlyThresher@gmail.com>
2016-09-19 21:38:14 -0400

    Created JavaFX Project

---
+++ WaterReport/src/controller/LoginScreenController.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by grizz on 9/19/2016.
+ */
+public class LoginScreenController {
+}
---
+++ WaterReport/src/model/Login.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by grizz on 9/19/2016.
+ */
+public class Login {
+}
