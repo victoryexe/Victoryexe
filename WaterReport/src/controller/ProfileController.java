f1f5df358047cdcfc74a59849793595c089327f1
grizzlyThresher <grizzlyThresher@gmail.com>
2016-09-29 19:03:43 -0400

    Made the Profile and Registration Controllers

---
+++ WaterReport/src/controller/ProfileController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+import model.User.User;
+
+/**
+ * Created by grizz on 9/27/2016.
+ */
+public class ProfileController {
+
+    private User currentUser;
+    private String userName;
+    private String address;
+    private String userID;
+
+    @FXML
+    public void initialize() {
+
+    }
+}
---
+++ WaterReport/src/controller/RegistrationController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by grizz on 9/27/2016.
+ */
+public class RegistrationController {
+
+
+    private String userName;
+    private String pass;
+    private String pass2;
+    private String authLevel;
+
+    @FXML
+    public void initialize() {
+
+    }
+}
