9b0275225909d88ce013ca2af516d878cc9893a6
Alexandra Durso <adurso3@gatech.edu>
2016-10-20 18:57:31 -0400

    Made changes to Location

--- WaterReport/src/model/report/Location.java
+++ WaterReport/src/model/report/Location.java
@@ -2,24 +2,50 @@ package model.report;
 
 /**
  * Created by Alexandra on 10/12/2016.
- * Temp class
  */
-public class Location {
-    // TODO
-    //Temporary constructor & getters
-    private String latitude;
-    private String longitude;
-    public Location (String latitude, String longitude) {
+public class Location implements Comparable<Location> {
+    private double latitude;
+    private double longitude;
+    public Location (double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public String getLatitude() {
+    public double getLatitude() {
         return latitude;
     }
 
-    public String getLongitude() {
+    public double getLongitude() {
         return longitude;
     }
-    // Will require GMap integration
+    // TODO Will require GMap integration
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Location)) {
+            return false;
+        }
+        return this.latitude == ((Location) obj).latitude
+                && this.longitude == ((Location) obj).longitude;
+    }
+
+    @Override
+    public int hashCode() {
+        return 97 * ((Double) latitude).hashCode()
+                + 211 * ((Double) longitude).hashCode();
+    }
+
+    public int compareTo(Location loc) {
+        if (this.latitude != loc.latitude) {
+            return Double.compare(this.latitude, loc.latitude);
+        } else {
+            return Double.compare(this.longitude, loc.longitude);
+        }
+    }
 }
