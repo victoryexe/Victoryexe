0a899b77b1d8e0aff7e1772e7e16ca237542a933
Alexandra Durso <adurso3@gatech.edu>
2016-10-31 22:36:08 -0400

    Made a LogList

---
+++ WaterReport/src/model/log/LogList.java
@@ -0,0 +1,118 @@
+package model.log;
+
+import model.Users.Account;
+import model.Users.Admin;
+import model.Users.Manager;
+import model.report.Report;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Alexandra on 10/31/2016.
+ */
+public class LogList {
+    private static List<BannedAccountLog> bannedAccountLog = new LinkedList<>();
+    private static List<DeletedAccountLog> deletedAccountLog = new LinkedList<>();
+    private static List<DeletedReportLog> deletedReportLog = new LinkedList<>();
+    private static List<LoginAttemptLog> loginAttemptLog = new LinkedList<>();
+    private static List<UnblockAccountLog> unblockAccountLog = new LinkedList<>();
+
+    private LogList() {
+    }
+
+    /**
+     * Gets the List of all BannedAccountLogs
+     * @return a List of all BannedAccountLogs
+     */
+    public static List<BannedAccountLog> getBannedAccountLog() {
+        return new LinkedList<BannedAccountLog>(bannedAccountLog);
+    }
+
+    /**
+     * Gets the List of all DeletedAccountLogs
+     * @return a List of all DeletedAccountLogs
+     */
+    public static List<DeletedAccountLog> getDeletedAccountLog() {
+        return new LinkedList<>(deletedAccountLog);
+    }
+
+    /**
+     * Gets the List of all DeletedReportLogs
+     * @return a List of all DeletedReportLogs
+     */
+    public static List<DeletedReportLog> getDeletedReportLog() {
+        return new LinkedList<>(deletedReportLog);
+    }
+
+    /**
+     * Gets the List of all LoginAttemptLogs
+     * @return a List of all LoginAttemptLogs
+     */
+    public static List<LoginAttemptLog> getLoginAttemptLog() {
+        return new LinkedList<>(loginAttemptLog);
+    }
+
+    /**
+     * Gets the List of all UnblockAccountLogs
+     * @return a List of all UnlockAccountLogs
+     */
+    public static List<UnblockAccountLog> getUnblockAccountLog() {
+        return new LinkedList<>(unblockAccountLog);
+    }
+
+    /**
+     * Makes a new BannedAccountLog with the given params and adds it to the
+     * appropriate list
+     * @param responsibleAccount the Admin banning an account
+     * @param bannedAccountID the userid of the banned account
+     */
+    public static void makeBannedAccountEntry(Admin responsibleAccount, String bannedAccountID) {
+        BannedAccountLog log = new BannedAccountLog(responsibleAccount, bannedAccountID);
+        bannedAccountLog.add(log);
+    }
+
+    /**
+     * Makes a new DeletedAccountLog with the given params and adds it to the
+     * appropriate list
+     * @param responsibleAccount the Admin deleting an account
+     * @param deletedAccountID the userid of the deleted account
+     */
+    public static void makeDeletedAccountEntry(Admin responsibleAccount, String deletedAccountID) {
+        DeletedAccountLog log = new DeletedAccountLog(responsibleAccount, deletedAccountID);
+        deletedAccountLog.add(log);
+    }
+
+    /**
+     * Makes a new DeletedReportLog with the given params and adds it to the
+     * appropriate list
+     * @param responsibleAccount the Manager deleting an account
+     * @param report the deleted Report
+     */
+    public static void makeDeletedReportEntry(Manager responsibleAccount, Report report) {
+        DeletedReportLog log = new DeletedReportLog(responsibleAccount, report);
+        deletedReportLog.add(log);
+    }
+
+    /**
+     * Makes a new LoginAttemptLog with the given params and adds it to the
+     * appropriate list
+     * @param responsibleAccount the Account logging in
+     * @param successStatus boolean representing the Account's login success
+     */
+    public static void makeLoginAttemptEntry(Account responsibleAccount, boolean successStatus) {
+        LoginAttemptLog log = new LoginAttemptLog(responsibleAccount, successStatus);
+        loginAttemptLog.add(log);
+    }
+
+    /**
+     * Makes a new UnblockAccountLog with the given params and adds it to
+     * the appropriate list
+     * @param responsibleAccount the Admin unblocking an Account
+     * @param unblockedAccountID the unblockedAccount's userid
+     */
+    public static void makeUnblockAccountEntry(Admin responsibleAccount, String unblockedAccountID) {
+        UnblockAccountLog log = new UnblockAccountLog(responsibleAccount, unblockedAccountID);
+        unblockAccountLog.add(log);
+    }
+}
