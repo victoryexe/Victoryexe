9e2dc9c6490544d6b3c4c06fcb1c002fe0331f99
DacorvynY <iceguy12@gmail.com>
2016-11-05 15:37:43 -0400

    Please dont break everything

--- WaterReport/src/controller/ProfileController.java
+++ WaterReport/src/controller/ProfileController.java
@@ -24,20 +24,16 @@ import static controller.MainController.restrictToNums;
  */
 public class ProfileController {
 
-    private TextField lastnametextbox;
-    private  TextField firstnametextbox;
-    private TextField streetaddresstextbox;
-    private TextField citytextbox;
-    private TextField statetextbox;
-    private TextField countrytextbox;
-    private TextField aptnumtextbox;
-    private TextField zipcodetextbox;
-    private TextField emailtextbox;
-    private ComboBox salutationcombobox;
-    private Button salutationedit;
-    private Button submit;
-    private Label currsalutation;
-    private Profile currProfile;
+    private final TextField lastnametextbox;
+    private final TextField firstnametextbox;
+    private final TextField streetaddresstextbox;
+    private final TextField citytextbox;
+    private final TextField statetextbox;
+    private final TextField countrytextbox;
+    private final TextField aptnumtextbox;
+    private final TextField zipcodetextbox;
+    private final TextField emailtextbox;
+    private final Profile currProfile;
 
     public ProfileController(TextField lastnametextbox, TextField firstnametextbox, TextField streetaddresstextbox,
                    TextField statetextbox, TextField countrytextbox, TextField citytextbox, TextField aptnumtextbox,
@@ -52,15 +48,15 @@ public class ProfileController {
         this.aptnumtextbox = aptnumtextbox;
         this.zipcodetextbox = zipcodetextbox;
         this.emailtextbox = emailtextbox;
-        this.salutationcombobox = salutationcombobox;
-        this.salutationedit = salutationedit;
-        this.submit = submit;
-        this.currsalutation = currsalutation;
+        ComboBox salutationcombobox1 = salutationcombobox;
+        Button salutationedit1 = salutationedit;
+        Button submit1 = submit;
+        Label currsalutation1 = currsalutation;
 
         restrictToNums(aptnumtextbox);
         restrictToNums(zipcodetextbox);
 
-        ArrayList<String> salutation = new ArrayList<String>();
+        ArrayList<String> salutation = new ArrayList<>();
         salutation.add("");
         salutation.add("Mr.");
         salutation.add("Ms.");
@@ -68,12 +64,7 @@ public class ProfileController {
         salutation.add("Dr.");
         populateComboBox(salutationcombobox, salutation);
         salutationcombobox.setVisible(false);
-        salutationcombobox.setOnMousePressed(new EventHandler<MouseEvent>(){
-            @Override
-            public void handle(MouseEvent event) {
-                salutationcombobox.requestFocus();
-            }
-        });
+        salutationcombobox.setOnMousePressed(event -> salutationcombobox.requestFocus());
         currProfile = new Profile(LoginScreenController.currUser);
         if (currProfile.getTitle() != null) {
             currsalutation.setText(currProfile.getTitle());
@@ -96,82 +87,76 @@ public class ProfileController {
                 zipcodetextbox.setText("" + currProfile.getAddress().getZip());
             }
         }
-        salutationedit.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                salutationedit.setVisible(false);
-                submit.setVisible(true);
-                currsalutation.setVisible(false);
-                salutationcombobox.setVisible(true);
-                if (currProfile.getTitle() != null) {
-                    switch (currProfile.getTitle()) {
-                        case "":
-                            salutationcombobox.setValue(salutation.get(0));
-                            break;
-                        case "Mr.":
-                            salutationcombobox.setValue(salutation.get(1));
-                            break;
-                        case "Ms.":
-                            salutationcombobox.setValue(salutation.get(2));
-                            break;
-                        case "Mrs.":
-                            salutationcombobox.setValue(salutation.get(3));
-                            break;
-                        case "Dr.":
-                            salutationcombobox.setValue(salutation.get(4));
-                            break;
-                        default:
-                            salutationcombobox.setValue(salutation.get(0));
-                            break;
-                    }
+        salutationedit.setOnAction(event -> {
+            salutationedit.setVisible(false);
+            submit.setVisible(true);
+            currsalutation.setVisible(false);
+            salutationcombobox.setVisible(true);
+            if (currProfile.getTitle() != null) {
+                switch (currProfile.getTitle()) {
+                    case "":
+                        salutationcombobox.setValue(salutation.get(0));
+                        break;
+                    case "Mr.":
+                        salutationcombobox.setValue(salutation.get(1));
+                        break;
+                    case "Ms.":
+                        salutationcombobox.setValue(salutation.get(2));
+                        break;
+                    case "Mrs.":
+                        salutationcombobox.setValue(salutation.get(3));
+                        break;
+                    case "Dr.":
+                        salutationcombobox.setValue(salutation.get(4));
+                        break;
+                    default:
+                        salutationcombobox.setValue(salutation.get(0));
+                        break;
                 }
-                setAllEditable(true);
             }
+            setAllEditable(true);
         });
-        submit.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                submit.setVisible(false);
-                salutationedit.setVisible(true);
-                currProfile.changeTitle((String) salutationcombobox.getValue());
-                currsalutation.setText(currProfile.getTitle());
-                currsalutation.setVisible(true);
-                salutationcombobox.setVisible(false);
-                setAllEditable(false);
-                currProfile.changeName(firstnametextbox.getText() + " " + lastnametextbox.getText());
-                int zip;
-                int apt;
-                if (!currProfile.getEmail().equals(emailtextbox.getText())) {
-                    UserList.updateMap(currProfile.getEmail(), emailtextbox.getText());
-                    currProfile.changeEmail(emailtextbox.getText());
-                }
-                if (currProfile.getAddress() == null) {
-                    if (zipcodetextbox.getText().equals("")) {
-                        zip = -1;
-                    } else {
-                        zip = Integer.valueOf(zipcodetextbox.getText());
-                    }
-                    if (aptnumtextbox.getText().equals("")) {
-                        apt = -1;
-                    } else {
-                        apt = Integer.valueOf(aptnumtextbox.getText());
-                    }
-                    currProfile.changeAddress(new Address(streetaddresstextbox.getText(), apt,
-                            citytextbox.getText(), statetextbox.getText(), zip, countrytextbox.getText()));
+        submit.setOnAction(event -> {
+            submit.setVisible(false);
+            salutationedit.setVisible(true);
+            currProfile.changeTitle((String) salutationcombobox.getValue());
+            currsalutation.setText(currProfile.getTitle());
+            currsalutation.setVisible(true);
+            salutationcombobox.setVisible(false);
+            setAllEditable(false);
+            currProfile.changeName(firstnametextbox.getText() + " " + lastnametextbox.getText());
+            int zip;
+            int apt;
+            if (!currProfile.getEmail().equals(emailtextbox.getText())) {
+                UserList.updateMap(currProfile.getEmail(), emailtextbox.getText());
+                currProfile.changeEmail(emailtextbox.getText());
+            }
+            if (currProfile.getAddress() == null) {
+                if (zipcodetextbox.getText().equals("")) {
+                    zip = -1;
                 } else {
-                    currProfile.getAddress().setStreet(streetaddresstextbox.getText());
-                    currProfile.getAddress().setCity(citytextbox.getText());
-                    currProfile.getAddress().setState(statetextbox.getText());
-                    if (zipcodetextbox.getText().equals("")) {
-                        currProfile.getAddress().setZip(-1);
-                    } else {
-                        currProfile.getAddress().setZip(Integer.valueOf(zipcodetextbox.getText()));
-                    }
-                    if (aptnumtextbox.getText().equals("")) {
-                        currProfile.getAddress().setApartmentNum(-1);
-                    } else {
-                        currProfile.getAddress().setApartmentNum(Integer.valueOf(aptnumtextbox.getText()));
-                    }
+                    zip = Integer.valueOf(zipcodetextbox.getText());
+                }
+                if (aptnumtextbox.getText().equals("")) {
+                    apt = -1;
+                } else {
+                    apt = Integer.valueOf(aptnumtextbox.getText());
+                }
+                currProfile.changeAddress(new Address(streetaddresstextbox.getText(), apt,
+                        citytextbox.getText(), statetextbox.getText(), zip, countrytextbox.getText()));
+            } else {
+                currProfile.getAddress().setStreet(streetaddresstextbox.getText());
+                currProfile.getAddress().setCity(citytextbox.getText());
+                currProfile.getAddress().setState(statetextbox.getText());
+                if (zipcodetextbox.getText().equals("")) {
+                    currProfile.getAddress().setZip(-1);
+                } else {
+                    currProfile.getAddress().setZip(Integer.valueOf(zipcodetextbox.getText()));
+                }
+                if (aptnumtextbox.getText().equals("")) {
+                    currProfile.getAddress().setApartmentNum(-1);
+                } else {
+                    currProfile.getAddress().setApartmentNum(Integer.valueOf(aptnumtextbox.getText()));
                 }
             }
         });
